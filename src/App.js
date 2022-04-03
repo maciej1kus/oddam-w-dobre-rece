c5114f4dc0819149d6a5e83b6d3af9640d10747f
Bartosz Polanczyk <bpolanczyk@sumologic.com>
2022-04-04 01:02:39 +0200

    Druga częśc uwag.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
+
 import { Header } from "./components/Header";
 import { Home } from "./components/Home/Home";
 import { Register } from "./components/Register";
@@ -7,13 +8,14 @@ import { Login } from "./components/Login";
 import { Logout } from "./components/Logout";
 import { NotFound } from "./components/NotFound";
 
-
+// Staraj się spójnie używać albo funkcji albo "strzałek" tworząc komponentów (nie ma lepszych czy gorszych, po prostu wybierz jedne dla spójności ;) )
 function App() {
   return (
     <HashRouter>
-        <>
+        <> {/* Ten fragment jest niepotrzebny, bo masz na zewnątrz HashRouter */}
           <Header />
             <Switch>
+                {/* Możesz spróbować przenieść ścieżki do JS-owego pliku konfigu bądź obiektu przed funkcją App */}
                 <Route exact path="/" component={Home}/>
                 <Route path='/rejestracja' component={Register} />
                 <Route path='/logowanie' component={Login} />
