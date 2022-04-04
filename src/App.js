bcd34a19d22baaf6e65d58d704290034aa967a0a
maciej1kus <maciej1kus@gmail.com>
2022-04-04 13:54:06 +0200

    header section v2 - modified structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
-import { Header } from "./components/Header";
+import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Register } from "./components/Register";
 import { Login } from "./components/Login";
