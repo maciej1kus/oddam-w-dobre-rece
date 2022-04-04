6c49e6cdd60f8f74efadd8cfc0bc46e72aacd4ba
maciej1kus <maciej1kus@gmail.com>
2022-04-04 15:49:22 +0200

    app.js v2 - modified structure

--- src/App.js
+++ src/App.js
@@ -8,21 +8,17 @@ import { Login } from "./components/Login";
 import { Logout } from "./components/Logout";
 import { NotFound } from "./components/NotFound";
 
-// Staraj się spójnie używać albo funkcji albo "strzałek" tworząc komponentów (nie ma lepszych czy gorszych, po prostu wybierz jedne dla spójności ;) )
-function App() {
+const App = () => {
   return (
     <HashRouter>
-        <> {/* Ten fragment jest niepotrzebny, bo masz na zewnątrz HashRouter */}
           <Header />
             <Switch>
-                {/* Możesz spróbować przenieść ścieżki do JS-owego pliku konfigu bądź obiektu przed funkcją App */}
                 <Route exact path="/" component={Home}/>
                 <Route path='/rejestracja' component={Register} />
                 <Route path='/logowanie' component={Login} />
                 <Route path='/wylogowano' component={Logout} />
                 <Route path='*' component={NotFound} />
             </Switch>
-        </>
     </HashRouter>
   );
 }
