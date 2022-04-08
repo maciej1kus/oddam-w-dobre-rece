3e68bc4384792f99798beb08eb07f5ff069d5bb9
maciej1kus <maciej1kus@gmail.com>
2022-04-08 15:33:44 +0200

    created JS object with information on individual organisations, foundations and donations

---
+++ src/components/Home/HomeWhoWeHelp/allData.js
@@ -0,0 +1,107 @@
+export const data = {
+    foundations: {
+        description: 'W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.',
+        items: [
+            {
+                title: 'Fundacja “Dbam o Zdrowie”',
+                purpose: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
+                what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+            },
+            {
+                title: 'Fundacja “Dla dzieci”',
+                purpose: 'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
+                what: 'ubrania, meble, zabawki'
+            },
+            {
+                title: 'Fundacja “Bez domu”',
+                purpose: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.',
+                what: 'ubrania, jedzenie, ciepłe koce'
+            },
+            {
+                title: 'Fundacja “AAA”',
+                purpose: 'Cel i misja: Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                what: 'ubrania, meble, poduszki, kołdry'
+            },
+            {
+                title: 'Fundacja “BBB”',
+                purpose: 'Cel i misja: Duis aute irure dolor in reprehenderit in voluptate velit esse.',
+                what: 'ubrania, jedzenie, ciepłe koce'
+            },
+            {
+                title: 'Fundacja “CCC”',
+                purpose: 'Cel i misja: Placerat orci nulla pellentesque dignissim enim sit.',
+                what: 'ubrania, zabawki, meble'
+            },
+            {
+                title: 'Fundacja “DDD”',
+                purpose: 'Cel i misja: Excepteur sint occaecat cupidatat non proident.',
+                what: 'ubrania, jedzenie, meble'
+            },
+            {
+                title: 'Fundacja “EEE”',
+                purpose: 'Cel i misja: Pellentesque elit ullamcorper dignissim cras cursus sit.',
+                what: 'ubrania, jedzenie, sprzęt AGD'
+            },
+            {
+                title: 'Fundacja “FFF”',
+                purpose: 'Cel i misja: Venenatis lectus magna fringilla urna porttitor rhoncus.',
+                what: 'ubrania, poduszki, ciepłe koce'
+            }
+        ]
+    },
+    organizations: {
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do incididunt ut labore et dolore magna aliqua. Volutpat vitae semper. Temporce id velit ut tortor.',
+        items: [
+            {
+                title: 'Organizacja "Lorem ipsum 1"',
+                purpose: 'Cel i misja: lorem ipsum  lorem ipsum  lorem ipsum.',
+                what: 'ipsum, dolor, sit, amet'
+            },
+            {
+                title: 'Organizacja "Lorem ipsum 2"',
+                purpose: 'Cel i misja: lorem ipsum lorem ipsum.',
+                what: 'ipsum, dolor'
+            },
+            {
+                title: 'Organizacja "Lorem ipsum 3"',
+                purpose: 'Cel i misja: lorem ipsum  lorem ipsum sit, amet lorem ipsum.',
+                what: 'ipsum, sit, amet'
+            },
+            {
+                title: 'Organizacja "Lorem ipsum 4"',
+                purpose: 'Cel i misja: lorem ipsum  lorem ipsum  lorem ipsum.',
+                what: 'ipsum, dolor, sit, amet'
+            },
+            {
+                title: 'Organizacja "Lorem ipsum 5"',
+                purpose: 'Cel i misja: lorem ipsum lorem ipsum, dolor sit amet.',
+                what: 'ipsum, dolor, amet'
+            },
+            {
+                title: 'Organizacja "Lorem ipsum 6"',
+                purpose: 'Cel i misja: lorem ipsum  lorem ipsum sit, amet lorem ipsum.',
+                what: 'ipsum, dolor, amet'
+            }
+        ]
+    },
+    local: {
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do incididunt ut labore et dolore magna aliqua. Volutpat vitae semper. Temporce id velit ut tortor.',
+        items: [
+            {
+                title: 'Zbiórka "Lorem ipsum 1"',
+                purpose: 'Platea dictumst vestibulum rhoncus est pellentesque elit.',
+                what: 'ipsum, dolor, sit, amet'
+            },
+            {
+                title: 'Zbiórka "Lorem ipsum 2"',
+                purpose: 'Vulputate odio ut enim blandit. Placerat orci nulla pellentesque.',
+                what: 'ipsum, dolor'
+            },
+            {
+                title: 'Zbiórka "Lorem ipsum 3"',
+                purpose: 'Tempor nec feugiat nisl pretium fusce id velit ut tortor.',
+                what: 'ipsum, sit, amet'
+            },
+        ]
+    }
+}
